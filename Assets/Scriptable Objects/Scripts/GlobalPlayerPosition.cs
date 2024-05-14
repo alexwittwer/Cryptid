8c7b34f50af47f1400729cd11d082e891a919a65
Alex Wittwer <wittweral@gmail.com>
2024-05-13 22:33:36 -0400

    overall, fix scenes, remove UI manager, health bar depends only on player stats now

--- Assets/Scriptable Objects/Scripts/GlobalPlayerPosition.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GlobalPlayerPosition", menuName = "Scriptable Objects/GlobalPlayerPosition")]
-public class GlobalPlayerPosition : ScriptableObject
-{
-    public static Vector3 PlayerPosition { get; set; } = new Vector3(0, 0, 0);
-}
